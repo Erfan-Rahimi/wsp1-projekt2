25dddc0a330055fdeee5a2218d92b1561ec01554
Erfan-Rahimi <erfan.rahimi@elev.ga.ntig.se>
2024-12-02 13:01:20 +0100

    2024-12-02

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 class App < Sinatra::Base
 
+    # Funktion som returnerar en databaskoppling
+    # Exempel på användning: db.execute('SELECT * FROM fruits')
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/fruits.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+    
+    # Routen gör en redirect till '/fruits'
     get '/' do
-        erb(:"index")
+        redirect('/fruits')
     end
 
+    # Routen hämtar alla frukter i databasen
+    get '/fruits' do
+        @fruits = db.execute('SELECT * FROM fruits')
+        p @fruits
+        erb(:"fruits/index")
+    end
 end
